60962c37282b7aa6935f00b22611dbcd980559b1
Valeriia <parfelyuk.v@gmail.com>
2024-08-28 16:58:00 +0100

    Refactor obfuscate function to use JSON input and improve file handling

---
+++ scr/obfuscate.py
@@ -0,0 +1,31 @@
+from data_parser import s3_path_parser as parser
+from file_reader import read_from_file
+from mask_sensitive_data import mask_sensitive_data
+from mask_sensitive_data import generate_masked_output
+import json as JSON
+
+
+def obfuscate(input: JSON, type_result: str) -> None:
+    """ This function obfuscates sensitive data in the file.
+
+    Args:
+        file_to_obfuscate: str
+        pii_fields: list of strings
+
+    Returns:
+    csv file with masked data or byte stream.
+
+    """
+    params = parser(input['file_to_obfuscate'])
+    dataFrame = read_from_file(params)
+    masked_df = mask_sensitive_data(dataFrame, input['pii_fields'])
+    result = generate_masked_output(masked_df,
+                                    type_result, params['file_format'])
+
+    return result
+
+
+obfuscate({
+    "file_to_obfuscate": 's3://test-bucket-270824/input/test.csv',
+    "pii_fields": ["Name", "Email"]
+}, 'byte-stream')
