dfc0e7067e41176b0a8de8ce922b022080d4a546
Valeriia <parfelyuk.v@gmail.com>
2024-08-28 16:57:15 +0100

    Refactor file_reader.py to use pandas for reading CSV and JSON files from S3

---
+++ scr/file_reader.py
@@ -0,0 +1,49 @@
+import boto3
+import botocore
+from io import StringIO
+import pandas as pd
+
+
+def read_from_S3(params: dict) -> None:
+    """Reads the file from an S3 bucket.
+    args:
+        params: dict
+    return:
+        data from the file read from S3
+    """
+
+    try:
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket=params['bucket_name'],
+                            Key=params['file_key'])
+        body = obj['Body'].read().decode('utf-8')
+        data = StringIO(body)
+        return data
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The file does not exist.")
+        else:
+            raise
+
+
+def read_from_file(params: dict) -> None:
+    """Reads a data from a file in S3.
+    args:
+        params: dict
+    return:
+        dataframe from the file read from S3
+    """
+    try:
+        data = read_from_S3(params)
+
+        if params['file_format'] == 'csv':
+            df = pd.read_csv(data)
+            return df
+        if params['file_format'] == 'json':
+            df = pd.read_json(data)
+            return df
+        else:
+            raise ValueError("The file format is not supported")
+    except Exception:
+        print("Error reading the file")
+        raise
