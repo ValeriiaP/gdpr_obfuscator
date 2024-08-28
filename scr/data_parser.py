7a3f06bc84e7b3801f19f0c17c5d2bf07781c862
Valeriia <parfelyuk.v@gmail.com>
2024-08-28 13:03:10 +0100

    Add data_parser module for parsing S3 paths

---
+++ scr/data_parser.py
@@ -0,0 +1,24 @@
+import urllib.parse as urlparse
+
+
+def s3_path_parser(s3_path: str) -> None:
+    """This function parse the s3 path into:
+    - bucket_name,
+    - file_key
+    - file format.
+
+    Args:
+        s3_path: str
+
+    Returns:
+    dictionary with bucket_name, file_key and file format.
+
+    """
+    parsed_url = urlparse.urlparse(s3_path)
+    bucket_name = parsed_url.netloc
+    file_key = parsed_url.path.lstrip('/')
+    file_format = file_key.split('.')[-1]
+
+    return {"bucket_name": bucket_name,
+            "file_key": file_key,
+            "file_format": file_format}
