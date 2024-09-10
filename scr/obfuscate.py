7e505b503a9ffd472de9c8a9a61e85ed3f521de0
Valeriia <parfelyuk.v@gmail.com>
2024-09-10 21:23:01 +0300

    Refactor obfuscate function

--- scr/obfuscate.py
+++ scr/obfuscate.py
@@ -1,11 +1,10 @@
-from data_parser import s3_path_parser as parser
-from file_reader import read_from_file
-from mask_sensitive_data import mask_sensitive_data
-from mask_sensitive_data import generate_masked_output
+from scr.data_parser import s3_path_parser as parser
+from scr.file_reader import read_from_file
+from scr.mask_sensitive_data import mask_sensitive_data, generate_masked_output
 import json as JSON
 
 
-def obfuscate(input: JSON, type_result: str) -> None:
+def obfuscate(input: JSON) -> bytes:
     """ This function obfuscates sensitive data in the file.
 
     Args:
@@ -13,19 +12,11 @@ def obfuscate(input: JSON, type_result: str) -> None:
         pii_fields: list of strings
 
     Returns:
-    csv, json file or byte stream with masked data.
-
+    byte stream with masked data.
     """
     params = parser(input['file_to_obfuscate'])
     dataFrame = read_from_file(params)
     masked_df = mask_sensitive_data(dataFrame, input['pii_fields'])
-    result = generate_masked_output(masked_df,
-                                    type_result, params['file_format'])
+    result = generate_masked_output(masked_df, params['file_format'])
 
     return result
-
-
-obfuscate({
-    "file_to_obfuscate": 's3://test-bucket-030924/input/test.json',
-    "pii_fields": ["Name", "Email"]
-}, 'byte-stream')
