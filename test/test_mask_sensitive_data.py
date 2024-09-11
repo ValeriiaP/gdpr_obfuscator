f493283ddc86e2e6f85ccf17fb91edf419c21bc1
Valeriia <parfelyuk.v@gmail.com>
2024-09-11 22:16:50 +0300

    Refactor test_mask_sensitive_data.py to improve obfuscation and handling of missing sensitive fields

--- test/test_mask_sensitive_data.py
+++ test/test_mask_sensitive_data.py
@@ -14,32 +14,32 @@ class TestMaskSensitiveData(unittest.TestCase):
                            'Email': {0: '***'}, 'Phone': {0: '123456789'}}
         self.assertEqual(mask_data(df, pii_fields).to_dict(), expected_result)
 
-    def test_obfuscation_if_some_pii_fields_missing(self):
+    @patch('logging.warning')
+    def test_obfuscation_if_some_pii_fields_missing(self, mock_warning):
         data = {'Name': ['Bob'], 'Email': ['bob@example.com']}
         df = pd.DataFrame(data)
         pii_fields = ['Name', 'Phone']
         expected_result = {'Name': {0: '***'}, 'Email': {0: 'bob@example.com'}}
-        self.assertEqual(mask_data(df, pii_fields).to_dict(), expected_result)
+        self.assertEqual(  (df, pii_fields).to_dict(), expected_result)
+        mock_warning.assert_called_once_with(
+            "Field 'Phone' not found in dataframe columns.")
 
-    @patch('logging.info')
     @patch('logging.warning')
-    def test_obfuscation_no_sensitive_fields_provided(self,
-                                                      mock_warning, mock_info):
-        data = {'Name': ['Olena'], 'Email': ['olena@example.com']}
-        df = pd.DataFrame(data)
-        pii_fields = []
-        expected_result = mask_data(df, pii_fields)
-        mock_info.assert_called_once_with('No sensitive fields provided.')
-        mock_warning.assert_not_called()
-        pd.testing.assert_frame_equal(expected_result, df)
-
-    @patch('logging.info')
-    @patch('logging.warning')
-    def test_no_sensitive_fields_in_dataframe(self, mock_warning, mock_info):
+    def test_no_sensitive_fields_in_dataframe(self, mock_warning):
         data = {'Name': ['Olena'], 'Email': ['olena@example.com']}
         df = pd.DataFrame(data)
         pii_fields = ['Phone', 'Address']
+        expected_calls = [
+            unittest.mock.call(
+                "Field 'Phone' not found in dataframe columns."),
+            unittest.mock.call(
+                "Field 'Address' not found in dataframe columns.")
+        ]
         expected_result = mask_data(df, pii_fields)
         self.assertEqual(mock_warning.call_count, 2)
-        mock_info.assert_not_called()
+        mock_warning.assert_has_calls(expected_calls, any_order=False)
         pd.testing.assert_frame_equal(expected_result, df)
+
+
+if __name__ == '__main__':
+    unittest.main()
